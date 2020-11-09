23c1375eb616f124844c9e6b4bc81da02bb9d408
Stanislav <stasccb@gmail.com>
2020-11-10 00:37:45 +0300

    init2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-//import reportWebVitals from './reportWebVitals';
+
 
 const Header = () => {
   return <h2>Hello world!</h2>
@@ -40,7 +40,3 @@ const App = () => {
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
